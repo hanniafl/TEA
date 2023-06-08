80bbd17005be8dbedcbd9722e4d6608899cb46ae
MIGUELHBK24 <miguel_valencia24@hotmail.com>
2023-06-08 10:15:59 -0600

    Video 30 terminado, CRUD para usuarios

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Spatie\Permission\Models\Role;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('admin.users.index');
+    }
+
+    public function edit(User $user)
+    {
+        $roles =Role::all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.edit', $user);
+    }
+}
---
+++ app/Http/Livewire/AdminUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+use Livewire\WithPagination;
+
+class AdminUsers extends Component
+{
+    use WithPagination;
+
+    protected $paginationTheme = "bootstrap";
+
+    public $search;
+
+    public function render()
+    {
+        $users = User::where('name', 'LIKE', '%' . $this->search . '%')
+                    ->orwhere('email', 'LIKE', '%' . $this->search . '%')
+                    ->paginate(8);
+
+        return view('livewire.admin-users', compact('users'));
+    }
+
+    public function limpiar_page(){
+        $this->reset('page');
+    }
+}
