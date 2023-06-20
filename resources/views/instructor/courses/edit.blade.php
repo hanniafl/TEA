35ff55b2f1fd590c317073ca8bebe4d7de364fe4
hanniafl <70921621+hanniafl@users.noreply.github.com>
2023-06-19 21:51:28 -0600

    plantilla para instructores

--- resources/views/instructor/courses/edit.blade.php
+++ resources/views/instructor/courses/edit.blade.php
@@ -1,50 +1,22 @@
-<x-app-layout>
+<x-instructor-layout>
 
-    <div class="container py-8 grid grid-cols-5">
+    <x-slot name="course">
+        {{$course->slug}}
 
-        <aside>
-            <h1 class="font-bold text-lg mb-4">Edicion del curso</h1>
+    </x-slot>
 
-            <ul class="text-sm text-gray-600">
-                <li class="leading-7 mb-1 border-l-4 border-indigo-400 pl-2">
-                    <a href="">Informacion del curso</a>
-                </li>
-
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Lecciones del curso</a>
-                </li>
-
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Metas del curso</a>
-                </li>
-
-                <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                    <a href="">Estudiantes</a>
-                </li>
-            </ul>
-        </aside>
-
-        <div class="col-span-4 card">
-
-            <div class="card-body text-gray-600">
-                <h1 class="text-2xl font-bold">INFORMACION DEL CURSO</h1>
-                <hr class="mt-2 mb-6">
-
-                {!! Form::model($course, ['route' => ['instructor.courses.update', $course], 'method' => 'put', 'files' => true]) !!}
-
-                    @include('instructor.courses.partials.form')
-
-                    <div class="flex justify-end">
-                        {!! Form::submit('Actualizar informacion', ['class' => 'btn btn-primary bg-blue-500 text-white cursor-pointer']) !!}
-                    </div>
-
-                {!! Form::close() !!}
-
-            </div>
-
+    <h1 class="text-2xl font-bold">INFORMACION DEL CURSO</h1>
+    <hr class="mt-2 mb-6">
+        
+    {!! Form::model($course, ['route' => ['instructor.courses.update', $course], 'method' => 'put', 'files' => true]) !!}
+        
+        @include('instructor.courses.partials.form')
+        
+        <div class="flex justify-end">
+            {!! Form::submit('Actualizar informacion', ['class' => 'btn btn-primary bg-blue-500 text-white cursor-pointer']) !!}
         </div>
-
-    </div>
+        
+    {!! Form::close() !!}
 
     <x-slot name="js">
 
@@ -53,4 +25,4 @@
 
     </x-slot>
 
-</x-app-layout>
+</x-instructor-layout>
--- routes/instructor.php
+++ routes/instructor.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Instructor\CourseController;
 use GuzzleHttp\Middleware;
+use App\Http\Livewire\Instructor\CoursesCurriculum;
 
 Route::redirect('', 'instructor/courses');
 
 Route::resource('courses', CourseController::class)->names('courses');
+
+Route::get('courses/{course}/curriculum', CoursesCurriculum::class)->name('courses.curriculum');
